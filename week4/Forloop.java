0ddd59e29864ab1c6bb7aadd1d776d907b0268a9
Aryanshah010 <shaharyan844@gmail.com>
2024-04-28 11:56:40 +0545

    Week 4

--- week4/Forloop.java
+++ week4/Forloop.java
@@ -3,7 +3,7 @@ package week4;
 public class Forloop {
     public static void main(String[] args) {
         for (int index = 0; index < 5; index++) {
-            System.out.println("Loop running" + index);
+            System.out.println("Loop running " + index);
         }
     }
 
